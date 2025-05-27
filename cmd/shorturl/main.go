9386c8cbf26327d9ccea7ce7ee75a1d0e9b2cbda
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2025-05-27 12:33:59 +0200

    read configuration from environment variables

--- cmd/shorturl/main.go
+++ cmd/shorturl/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"flag"
 	"net/http"
+	"os"
 
 	"github.com/hrashk/shorturl/internal/app"
 )
 
 func main() {
-	readConfigFromArgs()
+	readConfig()
 
 	err := http.ListenAndServe(app.GetListenAddr(), app.NewHandler())
 
@@ -17,10 +18,25 @@ func main() {
 	}
 }
 
-func readConfigFromArgs() {
-	listenAddr := flag.String("a", app.GetListenAddr(), "HTTP listen address")
-	redirectBaseURL := flag.String("b", app.GetRedirectBaseURL(), "Base URL for redirects")
-	flag.Parse()
-	app.SetListenAddr(*listenAddr)
-	app.SetRedirectBaseURL(*redirectBaseURL)
+func readConfig() {
+	fs := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+
+	var argListenAddr = fs.String("a", app.GetListenAddr(), "HTTP listen address")
+	var argRedirectBaseURL = fs.String("b", app.GetRedirectBaseURL(), "Base URL for redirects")
+
+	fs.Parse(os.Args[1:])
+
+	listenAddr := argOrEnv(argListenAddr, "SERVER_ADDRESS")
+	redirectBaseURL := argOrEnv(argRedirectBaseURL, "BASE_URL")
+
+	app.SetListenAddr(listenAddr)
+	app.SetRedirectBaseURL(redirectBaseURL)
+}
+
+func argOrEnv(argValue *string, envName string) string {
+	if envValue, ok := os.LookupEnv(envName); ok {
+		return envValue
+	} else {
+		return *argValue
+	}
 }
--- cmd/shorturl/main_test.go
+++ cmd/shorturl/main_test.go
@@ -5,16 +5,100 @@ import (
 	"testing"
 
 	"github.com/hrashk/shorturl/internal/app"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-func Test_readConfigFromArgs(t *testing.T) {
+type MainSuite struct {
+	suite.Suite
+	origArgs     []string
+	origListen   string
+	origRedirect string
+}
+
+func TestMainSuite(t *testing.T) {
+	suite.Run(t, &MainSuite{})
+}
+
+func (suite *MainSuite) SetupTest() {
+	suite.origArgs = os.Args
+	suite.origListen = app.GetListenAddr()
+	suite.origRedirect = app.GetRedirectBaseURL()
+}
+
+func (suite *MainSuite) TearDownTest() {
+	os.Args = suite.origArgs
+
+	app.SetListenAddr(suite.origListen)
+	app.SetRedirectBaseURL(suite.origRedirect)
+
+	os.Unsetenv("SERVER_ADDRESS")
+	os.Unsetenv("BASE_URL")
+}
+
+func (suite *MainSuite) Test_readConfig() {
 	const listen = "localhost:9999"
 	const redirect = "http://example.com:1024"
 	os.Args = []string{"", "-a", listen, "-b", redirect}
 
-	readConfigFromArgs()
+	readConfig()
 
-	assert.Equal(t, listen, app.GetListenAddr())
-	assert.Equal(t, redirect, app.GetRedirectBaseURL())
+	suite.Equal(listen, app.GetListenAddr())
+	suite.Equal(redirect, app.GetRedirectBaseURL())
+}
+
+func (suite *MainSuite) Test_readConfigWithDefaultListenAddress() {
+	const redirect = "http://example.com:1024"
+	os.Args = []string{"", "-b", redirect}
+
+	readConfig()
+
+	suite.Equal(suite.origListen, app.GetListenAddr())
+	suite.Equal(redirect, app.GetRedirectBaseURL())
+}
+
+func (suite *MainSuite) Test_readConfigWithDefaultRedirectAddress() {
+	const listen = "localhost:9999"
+	os.Args = []string{"", "-a", listen}
+
+	readConfig()
+
+	suite.Equal(listen, app.GetListenAddr())
+	suite.Equal(suite.origRedirect, app.GetRedirectBaseURL())
+}
+
+func (suite *MainSuite) Test_readConfig_Defaults() {
+	os.Args = []string{""}
+
+	readConfig()
+
+	suite.Equal(suite.origListen, app.GetListenAddr())
+	suite.Equal(suite.origRedirect, app.GetRedirectBaseURL())
+}
+
+func (suite *MainSuite) Test_readConfigWithEnvServerAddress() {
+	const envListen = "127.0.0.1:8088"
+	os.Setenv("SERVER_ADDRESS", envListen)
+
+	const argListen = "localhost:9999"
+	const argRedirect = "http://example.com:1024"
+	os.Args = []string{"", "-a", argListen, "-b", argRedirect}
+
+	readConfig()
+
+	suite.Equal(envListen, app.GetListenAddr())
+	suite.Equal(argRedirect, app.GetRedirectBaseURL())
+}
+
+func (suite *MainSuite) Test_readConfigWithEnvRedirectURL() {
+	const envRedirect = "http://random.com:4201"
+	os.Setenv("BASE_URL", envRedirect)
+
+	const argListen = "localhost:9999"
+	const argRedirect = "http://example.com:1024"
+	os.Args = []string{"", "-a", argListen, "-b", argRedirect}
+
+	readConfig()
+
+	suite.Equal(argListen, app.GetListenAddr())
+	suite.Equal(envRedirect, app.GetRedirectBaseURL())
 }
