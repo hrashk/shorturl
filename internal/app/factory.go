81b6b1ed5db218b359fd9fcb2f767e3430ad011d
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2025-05-30 15:05:49 +0200

    move the middleware to the logger file

--- internal/app/factory.go
+++ internal/app/factory.go
@@ -2,7 +2,6 @@ package app
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -34,59 +33,4 @@ func NewHandlerWithLogger(logger Logger) http.Handler {
 	})
 
 	return r
-}
-
-type wrapper struct {
-	Logger
-}
-
-func (lw wrapper) middleware(h http.Handler) http.Handler {
-	logFn := func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-
-		responseData := responseData{
-			status: 0,
-			size:   0,
-		}
-		rw := loggingResponseWriter{
-			ResponseWriter: w,
-			responseData:   responseData,
-		}
-		h.ServeHTTP(&rw, r)
-
-		duration := time.Since(start)
-
-		lw.Info("",
-			"uri", r.RequestURI,
-			"method", r.Method,
-			"status", responseData.status,
-			"duration", duration,
-			"size", responseData.size,
-		)
-	}
-
-	return http.HandlerFunc(logFn)
-}
-
-type (
-	responseData struct {
-		status int
-		size   int
-	}
-
-	loggingResponseWriter struct {
-		http.ResponseWriter
-		responseData responseData
-	}
-)
-
-func (r *loggingResponseWriter) Write(b []byte) (int, error) {
-	size, err := r.ResponseWriter.Write(b)
-	r.responseData.size += size
-	return size, err
-}
-
-func (r *loggingResponseWriter) WriteHeader(statusCode int) {
-	r.ResponseWriter.WriteHeader(statusCode)
-	r.responseData.status = statusCode
 }
--- internal/app/logger.go
+++ internal/app/logger.go
@@ -1,7 +1,9 @@
 package app
 
 import (
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/rs/zerolog"
 )
@@ -18,4 +20,59 @@ func NewZeroLogger() ZeroLogger {
 
 func (zl ZeroLogger) Info(msg string, fields ...any) {
 	zl.logger.Info().Fields(fields).Msg(msg)
+}
+
+type wrapper struct {
+	Logger
+}
+
+func (lw wrapper) middleware(h http.Handler) http.Handler {
+	logFn := func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		responseData := responseData{
+			status: 0,
+			size:   0,
+		}
+		rw := loggingResponseWriter{
+			ResponseWriter: w,
+			responseData:   responseData,
+		}
+		h.ServeHTTP(&rw, r)
+
+		duration := time.Since(start)
+
+		lw.Info("",
+			"uri", r.RequestURI,
+			"method", r.Method,
+			"status", responseData.status,
+			"duration", duration,
+			"size", responseData.size,
+		)
+	}
+
+	return http.HandlerFunc(logFn)
+}
+
+type (
+	responseData struct {
+		status int
+		size   int
+	}
+
+	loggingResponseWriter struct {
+		http.ResponseWriter
+		responseData responseData
+	}
+)
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode
 }
